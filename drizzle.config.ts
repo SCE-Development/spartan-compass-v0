05a1bf1e0a040d0e698529ad46b231e124625249
gursheyss <rkwarya@gmail.com>
2024-03-10 01:54:17 -0800

    some db setup

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'drizzle-kit';
+const { DATABASE_URL } = process.env;
+if (!DATABASE_URL) {
+	throw new Error('No url');
+}
+export default {
+	schema: './src/lib/db/schema.ts',
+	out: './src/lib/db/migrations',
+	driver: 'pg',
+	dbCredentials: {
+		connectionString: DATABASE_URL
+	}
+} satisfies Config;
