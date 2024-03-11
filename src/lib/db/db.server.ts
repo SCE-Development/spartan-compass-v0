f2e037ba1e8b14375af64b87cbd4b757221a14f2
gursheyss <rkwarya@gmail.com>
2024-03-10 17:24:52 -0700

    formatting

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -3,4 +3,4 @@ import postgres from 'postgres';
 import { DATABASE_URL } from '$env/static/private';
 
 const client = postgres(DATABASE_URL);
-export const db = drizzle(client, {});
+export const db = drizzle(client);
