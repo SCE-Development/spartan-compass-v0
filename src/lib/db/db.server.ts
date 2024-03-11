34d0849da245ef7cf7d14a702c97eaef6523f8e6
gursheyss <rkwarya@gmail.com>
2024-03-10 18:13:17 -0700

    fix sum stuff

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -1,6 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { DATABASE_URL } from '$env/static/private';
+const { DATABASE_URL } = process.env;
+if (!DATABASE_URL) {
+	throw new Error('No url');
+}
 
 const client = postgres(DATABASE_URL);
 export const db = drizzle(client);
