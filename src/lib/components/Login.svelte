e86830eb713e8a94f8a693c380a13a54eb229e81
Jonathan Nguyen <jonathanguven@gmail.com>
2024-03-13 16:30:28 -0700

    remove unused logic

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { goto } from '$app/navigation';
 
-	const login = () => {
-		goto('/auth');
-	};
 </script>
 
 <form method="POST" action="?/login" use:enhance class="m-32 flex justify-center">
 	<button class="btn" type="submit" name="action" value="login">Sign in with Google</button>
 </form>
+  
