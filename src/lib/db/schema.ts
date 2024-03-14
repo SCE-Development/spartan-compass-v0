5e95bcc9c9e19246c21352165fdbc9565ba98e4f
gursheyss <rkwarya@gmail.com>
2024-03-13 18:14:17 -0700

    merge schemas

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,13 @@
-import { pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import {
+	integer,
+	pgTable,
+	primaryKey,
+	serial,
+	text,
+	timestamp,
+	varchar
+} from 'drizzle-orm/pg-core';
 
 export const userTable = pgTable('users', {
 	id: varchar('id', { length: 15 }).primaryKey(),
@@ -18,3 +27,43 @@ export const sessionTable = pgTable('sessions', {
 		mode: 'date'
 	}).notNull()
 });
+
+export const professorsTable = pgTable('professors', {
+	id: serial('id').primaryKey(),
+	name: text('name').notNull(),
+	department: text('department').notNull()
+});
+
+export const coursesTable = pgTable('courses', {
+	id: serial('id').primaryKey(),
+	title: text('title').notNull(),
+	subject: text('subject').notNull(),
+	courseNumber: integer('course_number').notNull(),
+	description: text('description')
+});
+
+export const professorsCoursesTable = pgTable(
+	'professors_courses',
+	{
+		professorId: integer('professor_id')
+			.notNull()
+			.references(() => professorsTable.id),
+		courseId: integer('course_id')
+			.notNull()
+			.references(() => coursesTable.id)
+	},
+	(table) => ({
+		pk: primaryKey({ columns: [table.professorId, table.courseId] })
+	})
+);
+
+export const professorsCoursesRelations = relations(professorsCoursesTable, ({ one }) => ({
+	professor: one(professorsTable, {
+		fields: [professorsCoursesTable.professorId],
+		references: [professorsTable.id]
+	}),
+	course: one(coursesTable, {
+		fields: [professorsCoursesTable.courseId],
+		references: [coursesTable.id]
+	})
+}));
