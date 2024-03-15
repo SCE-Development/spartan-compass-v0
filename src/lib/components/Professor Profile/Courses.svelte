f67181d52a09705c89ba12be985337c28f41b875
Jonathan Nguyen <jonathanguven@gmail.com>
2024-03-15 04:18:32 -0700

    combine course subject and number in table

--- src/lib/components/Professor Profile/Courses.svelte
+++ src/lib/components/Professor Profile/Courses.svelte
@@ -17,9 +17,8 @@
     <thead>
       <tr>
         <th></th>
-        <th>Course</th>
-        <th>Subject</th>
         <th>Course Number</th>
+        <th>Course Name</th>
         <th>Description</th>
       </tr>
     </thead>
@@ -27,9 +26,8 @@
       {#each courses as course, i}
         <tr>
           <th>{i + 1}</th>
+          <td>{course.subject}-{course.courseNumber}</td>
           <td>{course.title}</td>
-          <td>{course.subject}</td>
-          <td>{course.courseNumber}</td>
           <td>{course.description}</td>
         </tr>
       {/each}
