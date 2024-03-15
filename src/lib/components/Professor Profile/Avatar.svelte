caae5c7e81f95872ed860d1bdf4ebc4a4c2f3ece
Jonathan Nguyen <jonathanguven@gmail.com>
2024-03-15 04:14:50 -0700

    initial implementation for professor and courses components

---
+++ src/lib/components/Professor Profile/Avatar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  interface Professor {
+    id: number;
+    name: string;
+    department: string;
+  }
+
+  let { professor } = $props<{ professor: Professor }>();
+  const initials: string = professor.name.split(' ').map((n) => n[0]).join('');
+</script>
+
+<div class="flex justify-center items-center rounded m-4">
+  <div class="flex w-full items-center overflow-hidden rounded-lg bg-zinc-300 px-8 py-6">
+    <div class="avatar placeholder mr-6">
+      <div class="bg-neutral text-neutral-content rounded-full w-24">
+        <span class="text-3xl text-gray-300">{initials}</span>
+      </div>
+    </div> 
+    <div class="flex-col justify-center">
+      <p class="badge rounded-md px-2 py-3">{professor.department}</p>
+      <div class="font-bold text-2xl mt-1 text-neutral">{professor.name}</div>
+    </div>
+  </div>
+</div>
---
+++ src/lib/components/Professor Profile/Courses.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { professorsCoursesRelations } from "$lib/db/schema";
+
+  interface Course {
+    id: number;
+    title: string;
+    subject: string;
+    courseNumber: number;
+    description: string | null;
+  }
+
+  let { courses } = $props<{ courses: Course[] }>();
+</script>
+
+<div class="overflow-x-auto">
+  <table class="table">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Course</th>
+        <th>Subject</th>
+        <th>Course Number</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each courses as course, i}
+        <tr>
+          <th>{i + 1}</th>
+          <td>{course.title}</td>
+          <td>{course.subject}</td>
+          <td>{course.courseNumber}</td>
+          <td>{course.description}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
