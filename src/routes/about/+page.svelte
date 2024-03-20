0b6ad9a66c719ee3d33a5b2e38bee982a2a73390
kevintsoii <kevin.tsoi44@gmail.com>
2024-03-19 22:35:04 -0700

    add about, login, logout pages

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<p>
+	This site is like Rate My Professors but for SJSU students. Instead of looking up professors, you
+	search classes to see who teaches them in a user-friendly UI, showing professor ratings and grade
+	distributions.
+</p>
