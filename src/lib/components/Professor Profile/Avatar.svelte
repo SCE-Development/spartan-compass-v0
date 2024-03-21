0f4c2b7a7e231d09ece0361f0c4b6fd80dfbfbd7
Jonathan <jonathanguven@gmail.com>
2024-03-21 02:01:42 -0700

    remove text-center class from layout file, ui changes to banner

--- src/lib/components/Professor Profile/Avatar.svelte
+++ src/lib/components/Professor Profile/Avatar.svelte
@@ -23,14 +23,14 @@
   const initials: string = professor.name.split(' ').map((n) => n[0]).join('');
 </script>
 
-<div class="flex justify-center items-center mb-4 w-full bg-zinc-300">
-  <div class="flex w-full items-center overflow-hidden rounded-lg px-8 py-6">
+<div class="flex mb-4 mt-2 w-full bg-zinc-300">
+  <div class="flex w-full items-center overflow-hidden xl:w-3/4 rounded-lg px-8 py-6">
     <div class="avatar placeholder mr-6">
       <div class="bg-base-100 text-neutral-content rounded-full w-24">
         <span class="text-3xl text-gray-300">{initials}</span>
       </div>
     </div> 
-    <div class="flex-col justify-center mr-6">
+    <div class="flex-col justify-start mr-6">
       <p class="badge rounded-md px-2 py-3">{professor.department}</p>
       <div class="font-bold text-2xl mt-1 text-neutral">{professor.name}</div>
     </div>
