371ef9ee13e1d7215b18e032c376d595f2575c61
liubrian267 <118577422+liubrian267@users.noreply.github.com>
2024-03-21 00:36:37 -0700

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,3 @@
-import { fail, redirect } from '@sveltejs/kit';
-import { lucia } from '$lib/server/auth';
-import { googleAuth } from '$lib/server/auth';
-import { generateState, generateCodeVerifier } from 'arctic';
 import { dev } from '$app/environment';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
@@ -18,9 +14,7 @@ const searchSchema = z.object({
 });
 
 export const load: PageServerLoad = async (event: RequestEvent) => {
-
 	const form = await superValidate(zod(searchSchema));
-	
 	if (event.locals.user) {
 		return {
 			user: event.locals.user,
@@ -30,41 +24,6 @@ export const load: PageServerLoad = async (event: RequestEvent) => {
 };
 
 export const actions: Actions = {
-	logout: async (event: RequestEvent) => {
-		if (!event.locals.session) {
-			return fail(401);
-		}
-		await lucia.invalidateSession(event.locals.session.id);
-		const sessionCookie = lucia.createBlankSessionCookie();
-		event.cookies.set(sessionCookie.name, sessionCookie.value, {
-			path: '.',
-			...sessionCookie.attributes
-		});
-		redirect(302, '/');
-	},
-	login: async (event) => {
-		const state = generateState();
-		const codeVerifier = generateCodeVerifier();
-		const url = await googleAuth.createAuthorizationURL(state, codeVerifier, {
-			scopes: ['profile', 'email']
-		});
-
-		event.cookies.set('google_state', state, {
-			path: '/',
-			secure: !dev,
-			httpOnly: true,
-			maxAge: 60 * 10
-		});
-
-		event.cookies.set('code_verifier', codeVerifier, {
-			path: '/',
-			secure: !dev,
-			httpOnly: true,
-			maxAge: 60 * 10
-		});
-
-		return redirect(302, url.toString());
-	},
 	search: async (request) => {
 		const form = await superValidate(request, zod(searchSchema));
     	console.log(form.data)
