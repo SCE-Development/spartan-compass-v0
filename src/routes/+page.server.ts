e1e92a70fb0b79ce25f8e035a10ba33466af92e9
liubrian267 <118577422+liubrian267@users.noreply.github.com>
2024-03-21 00:48:40 -0700

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,37 +1,11 @@
-import { dev } from '$app/environment';
-import { superValidate } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters';
 import type { RequestEvent } from '@sveltejs/kit';
-import type { Actions, PageServerLoad } from './$types';
-import { db } from '$lib/db/db.server';
-import { coursesTable } from '$lib/db/schema';
-import { and, eq } from 'drizzle-orm';
-import { z } from 'zod';
+import type { PageServerLoad } from './$types';
 
-const searchSchema = z.object({
-	courseName: z.string(),
-	courseNumber: z.string(),
-});
-
+// delete later
 export const load: PageServerLoad = async (event: RequestEvent) => {
-	const form = await superValidate(zod(searchSchema));
 	if (event.locals.user) {
 		return {
-			user: event.locals.user,
-			form
+			user: event.locals.user
 		};
 	}
-};
-
-export const actions: Actions = {
-	search: async (request) => {
-		const form = await superValidate(request, zod(searchSchema));
-    	console.log(form.data)
-		const courseName = form.data.courseName
-		const courseNum = Number(form.data.courseNumber)
-		const courseID = await db.select({id: coursesTable.id}).from(coursesTable).where(and(eq(coursesTable.subject, courseName), eq(coursesTable.courseNumber, courseNum)))
-		console.log(courseID)
-		throw redirect(302, `/course/${courseID[0].id}`)
-	}
-	
 };
