5a9818bfbd97758575977d6ee4eaae7f4c3ea1d4
liubrian267 <liubrian267@gmail.com>
2024-03-21 18:24:17 -0700

    search form

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,38 @@
-import type { RequestEvent } from '@sveltejs/kit';
+import { redirect, type Actions, type RequestEvent } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { zod } from 'sveltekit-superforms/adapters'
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms';
+import { db } from '$lib/db/db.server';
+import { coursesTable } from '$lib/db/schema';
+import { and, eq } from 'drizzle-orm';
 
+const schema = z.object({
+	courseSub: z.string(),
+	courseNum: z.string()
+});
 // delete later
 export const load: PageServerLoad = async (event: RequestEvent) => {
+	const form  = await superValidate(zod(schema));
 	if (event.locals.user) {
 		return {
-			user: event.locals.user
+			user: event.locals.user,
+			form
 		};
 	}
 };
+
+export const actions: Actions = {
+    search: async (request) => {
+        const form = await superValidate(request, zod(schema));
+        console.log(form.data)
+        const courseName = form.data.courseSub
+        const courseNum = Number(form.data.courseNum)
+        const courseID = await db.select({id: coursesTable.id}).from(coursesTable).where(and(eq(coursesTable.subject, courseName), eq(coursesTable.courseNumber, courseNum)))
+        console.log(courseID)
+        throw redirect(302, `/course/${courseID[0].id}`)
+    }
+    
+};
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import Search from '$lib/components/Search.svelte';
 	let { data } = $props();
 	let user = $derived(data.user);
+	let formData = $derived(data.form);
 </script>
 
+<Search {formData}/>
 {#if user}
 	<h1 class="mt-8 flex w-full justify-center text-3xl">Logged in as {user.username}</h1>
 {:else}
