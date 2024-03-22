4901af09c6896420ed9f06d7c8a56aebcfe749aa
liubrian267 <liubrian267@gmail.com>
2024-03-21 18:28:05 -0700

    added course/[id]

---
+++ src/routes/course/[id]/+page.server.ts
@@ -0,0 +1,63 @@
+import { db } from '$lib/db/db.server';
+import { coursesTable, professorsTable, ratingsTable } from '$lib/db/schema';
+import { eq, inArray } from 'drizzle-orm';
+
+export const load = async ({ params }) => {
+    const { id } = params;
+
+    //console.log(id);
+    const result = await db
+        .select({
+            id: coursesTable.id,
+            title: coursesTable.title,
+            subject: coursesTable.subject,
+            courseNumber: coursesTable.courseNumber,
+            description: coursesTable.description
+        })
+        .from(coursesTable)
+        .where(eq(coursesTable.id, Number(id)));
+    const reviews = await db
+        .select({
+            userId: ratingsTable.userId,
+            professorId: ratingsTable.professorId,
+            rating: ratingsTable.rating,
+            review: ratingsTable.review,
+            courseId: ratingsTable.courseId
+        })
+        .from(ratingsTable)
+        .where(eq(ratingsTable.courseId, Number(id)));
+
+    const professorIds = reviews.map((review) => review.professorId);
+
+    // Query professorsTable to get professor names
+    interface Professor {
+        id: number;
+        name: string;
+    }
+
+    let professors: Professor[] = [];
+    if (professorIds.length > 0) {
+        // Query professorsTable to get professor names
+        professors = await db
+            .select({
+                id: professorsTable.id,
+                name: professorsTable.name
+            })
+            .from(professorsTable)
+            .where(inArray(professorsTable.id, professorIds));
+    }
+
+    // Map professor names to reviews
+    const reviewsWithProfessorNames = reviews.map((review) => {
+        const professor = professors.find((professor) => professor.id === review.professorId);
+        return {
+            ...review,
+            professorName: professor ? professor.name : 'Unknown'
+        };
+    });
+
+    return {
+        courseData: result[0],
+        reviewData: reviewsWithProfessorNames
+    };
+};
---
+++ src/routes/course/[id]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    let { data } = $props();
+    console.log(data);
+    let course = $derived(data.courseData);
+    let reviews = $derived(data.reviewData);
+    $inspect(course);
+    $inspect(reviews);
+</script>
+
+<div>{course.id}</div>
+<div>{course.title}</div>
+<div>{course.subject}</div>
+<div>{course.courseNumber}</div>
+<div>{course.description}</div>
+
+<table class="table">
+    <tbody>
+        {#each reviews as review, i}
+            <tr>
+                <th>{i + 1}</th>
+                <td> {review.professorName}</td>
+                <td>{review.rating}</td>
+                <td>{review.review}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
