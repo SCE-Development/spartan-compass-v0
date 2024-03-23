8e574f65b1e206b9f21eebc73b0f42b9e2e420ac
Jonathan <jonathanguven@gmail.com>
2024-03-22 20:47:29 -0700

    use sveltekit expected errors

--- src/routes/professors/+page.server.ts
+++ src/routes/professors/+page.server.ts
@@ -2,15 +2,13 @@ import { db } from '$lib/db/db.server';
 import { professorsTable } from '$lib/db/schema';
 
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
 	const professors = await db.select().from(professorsTable).orderBy(professorsTable.name);
 
 	if (professors.length === 0) {
-		return {
-			status: 404,
-			error: new Error('No professors found')
-		};
+		error(404, 'No professors found');
 	}
 
 	return {
