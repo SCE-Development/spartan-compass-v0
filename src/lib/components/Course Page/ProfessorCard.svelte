077196952240a1350ab616a93b01672bf183c65c
brandonnguyenn27 <brandonnguyenn27@gmail.com>
2024-03-31 10:53:04 -0700

    formatting and linting

--- src/lib/components/Course Page/ProfessorCard.svelte
+++ src/lib/components/Course Page/ProfessorCard.svelte
@@ -15,15 +15,15 @@
 
 <div class="card w-full bg-base-300 shadow-xl">
 	<div class="card-body">
-		<div class="flex items-center justify-between">
+		<div class="flex items-center justify-between text-center">
 			<div>
-				<div class="flex-col text-center text-2xl font-semibold">
+				<div class="flex-col text-2xl font-semibold">
 					<p>{firstName}</p>
 					<p>{lastName}</p>
 				</div>
 			</div>
 
-			<div class="ml-2">
+			<div>
 				<div class="mb-2 flex h-24 items-center justify-center rounded-md border-2">
 					<span class="text-4xl">
 						{professor.averageRating ? professor.averageRating : 'N/A'}
