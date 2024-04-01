fcc1048f11ee10ca3dd29be3656135eae4b51513
Jonathan <jonathanguven@gmail.com>
2024-03-31 22:10:35 -0700

    Redirect to last visited Page after Auth (#41)
    * redirect to last visited page after callback redirect
    
    * format + lint

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { page } from '$app/stores';
+	import { get } from 'svelte/store';
 </script>
 
 <form method="POST" action="/login" use:enhance>
+	<input type="hidden" name="returnUrl" value={get(page).url.pathname} />
 	<slot />
 </form>
