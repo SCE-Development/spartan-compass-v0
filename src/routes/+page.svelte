a6489c3d3694f7a2589254504fd3c89cf30c09ef
Jonathan <jonathanguven@gmail.com>
2024-03-31 21:01:20 -0700

    brought back logged in as message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,13 @@
 	let formData = $derived(data.form);
 </script>
 
-<div class="flex h-screen items-center justify-center">
+{#if user}
+	<h1 class="my-8 flex w-full justify-center text-3xl">Logged in as {user.username}</h1>
+{:else}
+	<h1 class="my-8 flex w-full justify-center text-3xl">Not logged in</h1>
+{/if}
+
+<div class="flex justify-center">
 	<Search {formData} />
 </div>
-{#if user}
-	<h1 class="mt-8 flex w-full justify-center text-3xl">Logged in as {user.username}</h1>
-{:else}
-	<h1 class="mt-8 flex w-full justify-center text-3xl">Not logged in</h1>
-{/if}
+
