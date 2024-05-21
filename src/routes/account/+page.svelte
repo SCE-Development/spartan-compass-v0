a6c0caa5e614ac693c4bf10b29f2db2d8f6b7a3e
gursheyss <rkwarya@gmail.com>
2024-05-21 08:26:28 -0700

    add account page placeholder

---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let { data } = $props();
+	let { user } = $derived(data);
+</script>
+
+{#if user}
+	<main>
+		<h1 class="flex justify-center align-middle text-3xl">My Account</h1>
+		<p class="flex justify-center align-middle text-lg">Welcome, {user.username}</p>
+	</main>
+{:else}
+	<main>
+		<h1 class="flex justify-center align-middle text-3xl">Please log in to view this page</h1>
+	</main>
+{/if}
