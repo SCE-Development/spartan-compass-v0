7beb3ac79f603176ac45253b1aa8fa215e9c8594
gursheyss <rkwarya@gmail.com>
2024-06-13 00:07:02 -0700

    update drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,6 @@
+import { config } from 'dotenv';
+config();
+
 import type { Config } from 'drizzle-kit';
 const { DATABASE_URL } = process.env;
 if (!DATABASE_URL) {
@@ -5,9 +8,13 @@ if (!DATABASE_URL) {
 }
 export default {
 	schema: './src/lib/db/schema.ts',
+	dialect: 'postgresql',
 	out: './src/lib/db/migrations',
-	driver: 'pg',
+	migration: {
+		table: 'migrations',
+		schema: 'public'
+	},
 	dbCredentials: {
-		connectionString: DATABASE_URL
+		url: DATABASE_URL
 	}
-} satisfies Config;
+} as Config;
